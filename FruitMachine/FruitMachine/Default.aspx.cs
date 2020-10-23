84a2fd4d503d87614d9f5d3852aa96346c85f0de
Joseph Hanke <Jhanke@nescot.ac.uk>
2020-10-23 16:06:30 +0100

    First version

---
+++ FruitMachine/FruitMachine/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FruitMachine
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string[] images = new string[12];
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            images[0] = "Bar";
+            images[1] = "Bell";
+            images[2] = "Cherry";
+            images[3] = "Diamond";
+            images[4] = "HorseShoe";
+            images[5] = "Lemon";
+        }
+    }
+}
---
+++ FruitMachine/FruitMachine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FruitMachine.Global" Language="C#" %>
